f6d147b6f77abf0024fa78f3f25b82a5984cd6f0
s-kitawaki <b1a2b1o4@gmail.com>
2025-09-08 09:02:56 +0900

    Vercelビルドエラー解消（'Navigate' 削除）

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes , Route , Navigate} from "react-router-dom"
+import { BrowserRouter, Routes , Route } from "react-router-dom"
 import Layout from "./Layout";
 import { Home } from "./pages/Home";
 import NoteDetail from "./pages/NoteDetail";
